48075c0bebdec74fbcf6be4b8cc3fcffdcb81bb2
vishnuambat <149373628+vishnuambat@users.noreply.github.com>
2023-12-06 11:09:57 +0530

    Create Usermodel.js

---
+++ Usermodel.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    name: String,
+    email: String,
+    password: String
+});
+
+var User = mongoose.model('User', userSchema);
